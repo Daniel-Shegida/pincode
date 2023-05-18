6360942ec64c9e7b673fefec2b7a5c46a002d9f9
daniel shegida <den.shegida@gmail.com>
2023-05-18 11:15:34 +0300

    feat(google_fonts):added google_fonts

--- lib/presentation/res/styles/project_styles.dart
+++ lib/presentation/res/styles/project_styles.dart
@@ -1,35 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:untitled/presentation/res/colors/project_colors.dart';
 
 /// стили проекта
 abstract class ProjectStyles {
-  static TextStyle boldBlack32Lato = TextStyle(
-    fontSize: 32.sp,
-    fontWeight: FontWeight.bold,
-    color: ProjectColors.black,
+  static TextStyle boldBlack32Lato = GoogleFonts.lato(
+    textStyle: TextStyle(
+      fontSize: 32.sp,
+      fontWeight: FontWeight.bold,
+      color: ProjectColors.black,
+    ),
   );
 
-  static TextStyle regularPurple12Lato = TextStyle(
-    fontSize: 12.sp,
-    color: ProjectColors.purple,
+  static TextStyle regularPurple12Lato = GoogleFonts.lato(
+    textStyle: TextStyle(
+      fontSize: 12.sp,
+      color: ProjectColors.purple,
+    ),
   );
 
-  static TextStyle boldPurple32Lato = TextStyle(
-    fontSize: 32.sp,
-    fontWeight: FontWeight.bold,
-    color: ProjectColors.purple,
+  static TextStyle boldPurple32Lato = GoogleFonts.lato(
+    textStyle: TextStyle(
+      fontSize: 32.sp,
+      fontWeight: FontWeight.bold,
+      color: ProjectColors.purple,
+    ),
   );
 
-  static TextStyle boldWhite16Lato = TextStyle(
-    fontSize: 16.sp,
-    fontWeight: FontWeight.bold,
-    color: ProjectColors.white,
+  static TextStyle boldWhite16Lato = GoogleFonts.lato(
+    textStyle: TextStyle(
+      fontSize: 16.sp,
+      fontWeight: FontWeight.bold,
+      color: ProjectColors.white,
+    ),
   );
 
-  static TextStyle mediumBlack32Lato = TextStyle(
-    fontSize: 32.sp,
-    fontWeight: FontWeight.w500,
-    color: ProjectColors.black,
+  static TextStyle mediumBlack32Lato = GoogleFonts.lato(
+    textStyle: TextStyle(
+      fontSize: 32.sp,
+      fontWeight: FontWeight.w500,
+      color: ProjectColors.black,
+    ),
   );
 }
